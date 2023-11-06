381ffbbad23238237c2cb5040de0312878e09e41
cauanlsreis <cauanlucas.reis@gmail.com>
2023-11-06 10:53:18 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+//react, components, estáticos
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import './App.css';
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    //embaralhar as perguntas
+    dispatch({type: "REORDER_QUESTIONS"});
+  }, []);
+  
+  return (
+      
+      <div className="App">
+        <h1>Quiz de Programação</h1>
+        {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Playing" && <Question />}
+        {quizState.gameStage === "End" && <GameOver />}
+
+      </div>
+  );
+}
+
+export default App
